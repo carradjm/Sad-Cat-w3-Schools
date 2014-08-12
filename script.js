3006022a52e75ea105118635da22116d9ed5c233
Malcolm Carradine <malcolm.carradine@gmail.com>
2014-08-12 17:05:14 -0400

    Changes background to fire!

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
   $('head').replaceWith("<img src='http://explosionhub.com/wp-content/uploads/2012/07/1562-cute-little-cat.jpg'>");
   $('body').replaceWith("<article>Don't use w3 Schools or this cat will be sad :(</article>")
+  $('html').css ({ "background-image": "url(http://emptylighthouse.com/sites/default/files/Fire-awareness-fire-marshal-training1.jpg)", "background-size" : "100%"})
 });
